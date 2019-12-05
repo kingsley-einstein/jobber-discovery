f7ef41fca61b4b583da2c08d7e0334c22e43531f
Kingsley Victor <javaprodigy56@gmail.com>
2019-12-05 02:43:43 +0100

    initial commit

---
+++ src/main/java/com/jobs/discovery/JobsDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.jobs.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class JobsDiscoveryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JobsDiscoveryApplication.class, args);
+	}
+}
